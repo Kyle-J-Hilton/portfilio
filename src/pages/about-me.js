c5d7eb3d0bf4fd52eabab1bd9b25c9c3c068d936
Kyle-J-Hilton <106459128+Kyle-J-Hilton@users.noreply.github.com>
2022-09-30 19:42:22 +0100

    Add files via upload

---
+++ src/pages/about-me.js
@@ -0,0 +1,116 @@
+import Head from "next/head";
+import styles from "../../styles/AboutMe.module.css";
+import {
+  DiGithubBadge,
+  DiHtml5,
+  DiJsBadge,
+  DiCss3Full,
+  DiReact,
+  DiPython,
+  DiNpm,
+} from "react-icons/di";
+import { FaGoogle, FaLinkedin } from "react-icons/fa";
+
+export default function p5js() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Kyle Hilton Portfolio</title>
+        <meta name="description" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={styles.header}>
+        <h1 className={styles.pageName}>About Me</h1>
+      </div>
+
+      <main className={styles.main}>
+        <div className={styles.leftContainer}>
+          <div className={styles.businessCard}>
+            <p>Kyle James Hilton</p>
+            <p>Software Developer</p>
+            <a href="/">Go back to home page</a>
+          </div>
+        </div>
+        <div className={styles.rightContainer}>
+          <div className={styles.box} id={styles.box1}>
+            <h3>Languages and Libraries I Use</h3>
+            <div className={styles.languageLists}>
+              <dl className={styles.iconList}>
+                <dt>
+                  {" "}
+                  <DiHtml5 />
+                </dt>
+                <dt>
+                  {" "}
+                  <DiCss3Full />
+                </dt>
+                <dt>
+                  {" "}
+                  <DiJsBadge />
+                </dt>
+              </dl>
+              <dl className={styles.iconList}>
+                <dt>
+                  {" "}
+                  <DiPython />
+                </dt>
+                <dt>
+                  {" "}
+                  <DiReact />
+                </dt>
+                <dt>
+                  {" "}
+                  <DiNpm />
+                </dt>
+              </dl>
+              <dl className={styles.list}>
+                <dt> Materials UI</dt>
+                <dt> @solana/web3</dt>
+                <dt> TypeScript</dt>
+                <dt> C++</dt>
+                <dt> storybook</dt>
+              </dl>
+            </div>
+          </div>
+          <div className={styles.box} id={styles.box2}>
+            <p>
+              I am a Software Developer who specializes in front-end Dev work. I
+              have learned coding through Codecademy's full-stack Engineering
+              bootcamp, and while studying Physics in college I learned C++.
+            </p>
+          </div>
+          <div className={styles.box} id={styles.box3}>
+            <h3>Contact Me</h3>
+            <form className={styles.form}>
+              <label>
+                Name:
+                <input type="text" name="name" />
+              </label>
+              <label>
+                Email:
+                <input type="text" name="email" />
+              </label>
+            </form>
+            <a href="https://www.linkedin.com/in/kyle-hilton-27a1b2199/">
+              {" "}
+              <FaLinkedin className={styles.iconList} />
+              Linkden
+            </a>
+            <a href="https://github.com/Kyle-J-Hilton">
+              {" "}
+              <DiGithubBadge className={styles.iconList} />
+              Github
+            </a>
+            <a href="https://gmail.com">
+              {" "}
+              <FaGoogle className={styles.iconList} /> Gmail
+              kylehilton18@gmail.com
+            </a>
+          </div>
+        </div>
+      </main>
+
+      <footer className={styles.footer}></footer>
+    </div>
+  );
+}

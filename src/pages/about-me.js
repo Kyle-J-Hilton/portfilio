2ffbd52ec479a5b629be742da78f0a3c2808d89d
Kyle-J-Hilton <106459128+Kyle-J-Hilton@users.noreply.github.com>
2022-10-02 22:15:58 +0100

    Update about-me.js

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -1,107 +1,229 @@
-import Head from "next/head";
-import styles from "../../styles/AboutMe.module.css";
-import {
-  DiGithubBadge,
-  DiHtml5,
-  DiJsBadge,
-  DiCss3Full,
-  DiReact,
-  DiPython,
-  DiNpm,
-} from "react-icons/di";
-import { FaGoogle, FaLinkedin } from "react-icons/fa";
+@font-face {
+    font-family: Humble_Lettering;
+    src: url('../public/Humble_Lettering.ttf');
+  }
 
-export default function p5js() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Kyle Hilton Portfolio</title>
-        <meta name="description" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className={styles.header}>
-        <h1 className={styles.pageName}>About Me</h1>
-      </div>
+  @font-face {
+    font-family: IceCreamPartySolid;
+    src: url('../public/IceCreamPartySolid.ttf');
+  }
 
-      <main className={styles.main}>
-        <div className={styles.leftContainer}>
-          <div className={styles.businessCard}>
-            <p>Kyle James Hilton</p>
-            <p>Software Developer</p>
-            <a href="/">Go back to home page</a>
-          </div>
-        </div>
-        <div className={styles.rightContainer}>
-          <div className={styles.box} id={styles.box1}>
-            <h3>Languages and Libraries I Use</h3>
-            <div className={styles.languageLists}>
-              <dl className={styles.iconList}>
-                <dt>
-                  {" "}
-                  <DiHtml5 />
-                </dt>
-                <dt>
-                  {" "}
-                  <DiCss3Full />
-                </dt>
-                <dt>
-                  {" "}
-                  <DiJsBadge />
-                </dt>
-              </dl>
-              <dl className={styles.iconList}>
-                <dt>
-                  {" "}
-                  <DiPython />
-                </dt>
-                <dt>
-                  {" "}
-                  <DiReact />
-                </dt>
-                <dt>
-                  {" "}
-                  <DiNpm />
-                </dt>
-              </dl>
-              <dl className={styles.list}>
-                <dt> Materials UI</dt>
-                <dt> @solana/web3</dt>
-                <dt> TypeScript</dt>
-                <dt> C++</dt>
-                <dt> storybook</dt>
-              </dl>
-            </div>
-          </div>
-          <div className={styles.box} id={styles.box2}>
-            <p>
-              I am a Software Developer who specializes in front-end Dev work. I
-              have learned coding through Codecademy's full-stack Engineering
-              bootcamp, and while studying Physics in college I learned C++.
-            </p>
-          </div>
-          <div className={styles.box} id={styles.box3}>
-            <h3>Contact Me</h3>
-            
-            <a href="https://www.linkedin.com/in/kyle-hilton-27a1b2199/">
-              {" "}
-              <FaLinkedin className={styles.iconList} />
-              Linkden
-            </a>
-            <a href="https://github.com/Kyle-J-Hilton">
-              {" "}
-              <DiGithubBadge className={styles.iconList} />
-              Github
-            </a>
-            <a href="https://gmail.com">
-              {" "}
-              <FaGoogle className={styles.iconList} /> Gmail
-              kylehilton18@gmail.com
-            </a>
-          </div>
-        </div>
-      </main>
+@media (max-width: 400px) {
 
-      <footer className={styles.footer}></footer>
-    </div>
-  );
+  .container {
+    padding: 0 2rem;
+    
+    background-color: rgb(198, 198, 198);
+   
+  }
+
+  .header {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    color: blue;
+    text-align: center;
+    justify-content: center;
+  }
+
+  .header a {
+    color: yellow;
+    margin: 33.5px;
+  }
+
+  .leftContainer {
+    height: 100%;
+    width: 50%;
+    
+  }
+
+   .businessCard {
+      font-size: 15px;
+      font-family: 'Humble_Lettering', sans-serif;
+   }
+
+  .rightContainer {
+    height: 100%;
+    width: 50%;
+    margin-left: 35%;
+ 
+  }
+
+  .box {
+      display: flex;
+      flex-direction: column;
+      border: 1px solid #eded81;
+      box-shadow: 6px 8px 4px 2px #3c3c3c57;
+      height: 200px;
+      width: 200px;
+     
+  }
+
+  #box1{
+   font-size: 8px;
+   font-family: 'Humble_Lettering', sans-serif;
+   background-image: url('../public/Images/lxpic.jpg');
+   background-repeat: no-repeat;
+   background-position: 50% 0;
+   background-size: cover;
+   z-index: 1;
+  }
+
+  #box2{
+    font-size: 18px; 
+    background-image: url('../public/Images/surfpic.jpg');
+    background-repeat: no-repeat;
+    background-position: 50% 0;
+    background-size: cover;
+  }
+
+  #box3{
+    font-size: 20px; 
+    background-image: url('../public/Images/nccpic.jpg');
+    background-repeat: no-repeat;
+    background-position: 50% 0;
+    background-size: cover;
+  }
+
+  .languageLists {
+      font-size: 9px;
+      justify-content: space-between;
+      align-content: space-between;
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
+  }
+
+  .form {
+      display: flex;
+      flex-direction: column;
+  }
+
+  .iconList {
+    font-size: 25px;
+    margin-top: -5px;
+  }
+
+  .list {
+    font-size: 12px;
+    
+  }
+}
+
+@media (min-width: 401px) {
+
+  .container {
+    
+    padding: 0 2rem;
+    height: 600px;
+    background-color: rgb(198, 198, 198);
+    
+  }
+
+  .header {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    color: blue;
+    text-align: center;
+    justify-content: center;
+  }
+
+  .header a {
+    color: yellow;
+    margin: 33.5px;
+  }
+
+  .leftContainer {
+    height: 100%;
+    width: 50%;
+  }
+
+   .businessCard {
+      font-size: 15px;
+      font-family: 'Humble_Lettering', sans-serif;
+      margin-top: -45px;
+   }
+
+  .rightContainer {
+    height: 100%;
+    width: 50%;
+    margin-left: 35%;
+  }
+
+  .box {
+      display: flex;
+      flex-direction: column;
+      border: 1px solid #eded81;
+  
+      box-shadow: 6px 8px 4px 2px #3c3c3c57;
+      height: 200px;
+      width: 200px;
+     
+  }
+
+  #box1{
+   font-size: 8px;
+   font-family: 'Humble_Lettering', sans-serif;
+   margin: -450px 0 0 150px;
+   transform: rotate(20deg);
+   background-image: url('../public/Images/lxpic.jpg');
+   background-repeat: no-repeat;
+   background-position: 50% 0;
+   background-size: cover;
+   z-index: 1;
+  }
+
+  #box2{
+    font-size: 18px; 
+    background-image: url('../public/Images/surfpic.jpg');
+    background-repeat: no-repeat;
+    background-position: 50% 0;
+    background-size: cover;
+    margin: -75px 0 0 300px;
+    transform: rotate(-15deg);
+    z-index: 9;
+  }
+
+  #box3 {
+    font-size: 10px; 
+    margin: -125px 0 0 50px;
+    transform: rotate(2deg);
+    background-image: url('../public/Images/nccpic.jpg');
+    background-repeat: no-repeat;
+    background-position: 50% 0;
+    background-size: cover;
+  }
+
+  .languageLists {
+      font-size: 9px;
+      
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
+  }
+
+  .form {
+      display: flex;
+      flex-direction: column;
+  }
+
+  .iconList {
+    font-size: 35px;
+    margin-right: 10px;
+  }
+
+  .list {
+    font-size: 18px; 
+    margin-top: 40px;
+    
+  }
+
+  .paragraph {
+    width: 75%;
+  }
+
+  .icon {
+    font-size: 40px;
+  }
 }
